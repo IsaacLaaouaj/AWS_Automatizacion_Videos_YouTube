ca2327d41f5c8d441832180e1f893d8376359b98
IsaacLaaouaj <isaac.laaouaj@gmail.com>
2024-01-11 17:47:48 +0100

    Creacion_scrpit_Guion_LLM

---
+++ creacion_guion_LLM.py
@@ -0,0 +1,26 @@
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------
+#                                           Generar guiones mediante LLM (GPT 2 Medium, de openAI, disponible solo en inglés):
+# --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+from transformers import pipeline
+
+titulo = input("Write the main theme or a title for your video: ")
+
+
+pipe = pipeline("text-generation", model="gpt2-medium") # Este modelo de LLM solo genera texto en inglés :(
+max_length = 100  
+num_return_sequences = 1
+
+generated_text = pipe (
+        f"Write the content of a video, with the title: {titulo}",
+        max_length=max_length,
+        num_return_sequences=num_return_sequences,
+        do_sample=True,  
+        pad_token_id=50256,  # Identificador de token de relleno para GPT-2
+    )
+
+
+
+
